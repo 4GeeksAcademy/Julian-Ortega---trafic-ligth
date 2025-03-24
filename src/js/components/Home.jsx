069be19753695691fc1506c1217096e431d0c982
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-03-24 14:10:04 -0600

    Update Home.jsx

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const TrafficLight = () => {
+  const [color, setColor] = useState("red");
+  const [colors, setColors] = useState(["red", "yellow", "green"]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const cycleColors = () => {
+    const currentIndex = colors.indexOf(color);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    setColor(colors[nextIndex]);
+  };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const addPurple = () => {
+    if (!colors.includes("purple")) {
+      setColors([...colors, "purple"]);
+    }
+  };
+
+  return (
+    <div className="d-flex flex-column align-items-center mt-5">
+      <div className="bg-dark p-3 rounded d-flex flex-column align-items-center">
+        {colors.map((light) => (
+          <div
+            key={light}
+            className={`rounded-circle my-2 ${
+              color === light ? "glow" : ""
+            }`}
+            style={{
+              width: "80px",
+              height: "80px",
+              backgroundColor: light,
+              cursor: "pointer",
+            }}
+            onClick={() => setColor(light)}
+          ></div>
+        ))}
+      </div>
+      <button className="btn btn-primary mt-3" onClick={cycleColors}>
+        Cambiar Color
+      </button>
+      <button className="btn btn-secondary mt-2" onClick={addPurple}>
+        Agregar Púrpura
+      </button>
+    </div>
+  );
 };
 
-export default Home;
+// Estilos para resaltar la luz seleccionada
+const styles = document.createElement("style");
+styles.innerHTML = `
+  .glow {
+    box-shadow: 0px 0px 20px 5px rgba(255, 255, 255, 0.8);
+  }
+`;
+document.head.appendChild(styles);
+
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
